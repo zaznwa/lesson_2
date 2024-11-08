a494c5ae9d367b907b29dfc2c2b1777f7b70ecd4
zaznwa <talipovazamat2@gmail.com>
2024-11-08 17:07:27 +0600

    Initial commit

---
+++ src/HW2.java
@@ -0,0 +1,20 @@
+public class HW2 {
+    public static void main(String[] args) {
+        System.out.println(permission(19, 15));
+        System.out.println(permission(18, -12));
+        System.out.println(permission(50, 10));
+        System.out.println(permission(36, -5));
+        System.out.println(permission(68, 20));
+    }
+
+    public static String permission(int age, int temperature) {
+        if ((age > 20 && age < 45) && (temperature > -20 && temperature < 30)) {
+            return ("Можно идти гулять");
+        } else if (age < 20 && (temperature > 0 && temperature < 28)) {
+            return ("Можно идти гулять");
+        }else if (age > 45 && (temperature > -10 && temperature < 25)){
+            return ("Можно идти гулять");
+        }else return ("Оставайтесь дома");
+    }
+
+}
